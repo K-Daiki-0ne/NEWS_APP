8c273b49cd3f3b06f1798a388fe3c08a77cada6c
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-05 21:22:26 +0900

    modified: add .env

--- src/function/config/url.ts
+++ src/function/config/url.ts
@@ -4,8 +4,8 @@
  */
 
 export function reqUrl (): string {
-  const newsUrl:string = '';
-  const apiKey: string | undefined = '';
-  const reqUrl: string = `${newsUrl}/${apiKey}`;
+  const newsUrl:string = 'http://newsapi.org/v2/top-headlines?country=jp&apiKey';
+  const apiKey: string | undefined = process.env.NEWS_API_KEY;
+  const reqUrl: string = `${newsUrl}=${apiKey}`;
   return reqUrl;
 }
