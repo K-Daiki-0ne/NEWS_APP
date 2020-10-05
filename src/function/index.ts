efe6b32a7dfa38147754cb1480cecd0a74392ee3
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-05 21:13:55 +0900

    add: function type

--- src/function/index.ts
+++ src/function/index.ts
@@ -7,7 +7,7 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.env.PORT || 3001;
 
-async function main() {
+async function main(): Promise<void> {
   try {
     await app.prepare();
     const server: Application = express();
