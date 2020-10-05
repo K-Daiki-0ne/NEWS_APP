ef3d4c87fe2d86e9570cd356527853c8340324cb
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-05 21:10:15 +0900

    modified: news routet

--- src/function/router/router.ts
+++ src/function/router/router.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
+import newsRouter from './v1/index';
 
 const router: Router = express.Router();
-router.use('/');
+router.use('/', newsRouter);
 
 export default router;
--- src/function/router/v1/index.ts
+++ src/function/router/v1/index.ts
@@ -1,7 +1,8 @@
 import express, { Router } from 'express';
+import { get } from '../../controller/newsController';
 
 const newsRouter: Router = express.Router();
 
-newsRouter.get('/');
+newsRouter.get('/', get);
 
 export default newsRouter;
