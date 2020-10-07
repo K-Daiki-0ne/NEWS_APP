175fa8c44fe00f9403ad2ebb6f0381372b9e8291
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-07 22:36:32 +0900

    modified: return data structure

--- src/api/news.ts
+++ src/api/news.ts
@@ -3,6 +3,5 @@ import axios from 'axios';
 export const newsRequest = async() => {
   const url: string = 'http://localhost:3001/';
   const response = await axios.get(url);
-  console.log(response);
-  return response;
+  return response.data;
 }
