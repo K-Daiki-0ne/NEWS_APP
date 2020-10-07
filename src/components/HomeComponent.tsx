3ae411a8b88a93df8e413f554f51192c42aabd6b
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-07 22:23:21 +0900

    add: base HomeComponent

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { newsRequest } from '../api/news';
 import CardComponent from './CardComponent/CardComponent'
 
 export const HomeCompoent:React.FC = (): JSX.Element => {
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    newsRequest()
+    .then((data: any) => setNews(data))
+    .catch(() => console.log('API Connect ...NO'));
+  }, [])
+
   return (
     <div >
       <CardComponent />
