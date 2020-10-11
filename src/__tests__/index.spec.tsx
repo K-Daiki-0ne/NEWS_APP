75c142017c626fe606976d4d76fc17812dab1aeb
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-11 17:49:52 +0900

    create: pages/index.tsx component rendering test code

--- src/__tests__/index.spec.tsx
+++ src/__tests__/index.spec.tsx
@@ -3,6 +3,14 @@ import { shallow } from 'enzyme';
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from '../theme/theme';
 import Home from '../../pages/index';
+import { HomeCompoent } from '../components/HomeComponent';
+import { 
+  AppBar,
+  Container,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
+
 
 describe('Root component rendering test', () => {
   const wrapper = shallow(
@@ -10,7 +18,38 @@ describe('Root component rendering test', () => {
       <Home />
     </ThemeProvider>
   );
+
   test('snapshot test', () => {
     expect(wrapper).toMatchSnapshot();
-  })
-})
+  });
+
+  describe('rendering test', () => {
+    const wrap = wrapper.find('div');
+    test('root', () => expect(wrap).toBeTruthy());
+
+    test('materil-ui header component', () => {
+      const header = wrap.find(AppBar);
+      expect(header).toBeTruthy();
+    });
+
+    test('material-ui Toolbar component', () => {
+      const toolbar = wrap.find(Toolbar);
+      expect(toolbar).toBeTruthy();
+    })
+
+    test('material-ui Typography component', () => {
+      const typography = wrap.find(Typography);
+      expect(typography).toBeTruthy();
+    })
+
+    test('material-ui contaienr conponent', () => {
+      const contaienr = wrap.find(Container);
+      expect(contaienr).toBeTruthy();
+    });
+
+    test('Child component', () => {
+      const home = wrap.find(Home);
+      expect(home).toBeTruthy();
+    })
+  });
+});
